d96b1278dc1d0d7b16e11b1293133390628d6dc3
Geovanny Mendoza <mgeovanny@uninorte.edu.co>
2025-01-11 00:38:36 -0500

    V[0.0.1] Add structure project

---
+++ src/main/java/com/goevannycode/AppWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.goevannycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goevannycode/AppWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goevannycode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
